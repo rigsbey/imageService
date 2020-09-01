4bc2de4ffad5009120600f13913864cf2ad7bff3
rigsbey <ab.kam@yandex.ru>
2020-09-01 12:13:42 +0400

    get last image

--- main.py
+++ main.py
@@ -17,20 +17,18 @@ def upload_img(request):
     template = open(os.getcwd() + "/templates/index.html")
     if request.files:
         args = request.files
-        byte_file = args.getlist('file')[0][1]
-        original_image = Image.open(io.BytesIO(byte_file))
+        byte_image = args.getlist('file')[0][1]
+        original_image = Image.open(io.BytesIO(byte_image))
         original_image.thumbnail((256, 256), Image.ANTIALIAS)
-        global ready_image
-        ready_image = original_image.save("./static/img/" + str(uuid.uuid4()) + ".png")
-    # todo добавить else(???)
+        # if(os.path.isfile("./static/img/" + str(uuid.uuid4()) + ".png")):
+        original_image.save("./static/img/" + str(uuid.uuid4()) + ".png")
     return response.html(template.read())
-
 
 @app.route(methods=['GET', 'POST'], uri='/images')
 def return_img(request):
     filenames = os.listdir(os.getcwd() + "/static/img/")
     for elem in filenames:
-        # return response.text(body=elem)
-        return jinja.render("return_image.html", request, img_path=os.getcwd() + "/static/img/" + elem)
+        return jinja.render("return_image.html", request, img_path="/static/img/" + elem)
+
 
 app.run(host="0.0.0.0", port=8000, debug=True)
