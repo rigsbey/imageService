066fa380c75b470e82fab26e97dea08c5003117d
rigsbey <ab.kam@yandex.ru>
2020-09-03 13:25:46 +0400

    fixed adding to db

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import io
 import os
+import sys
 import uuid
 
+import aio_pika
 from PIL import Image
+from gino import Gino
 from sanic import Sanic, response
 from sanic_jinja2 import SanicJinja2
 
@@ -11,7 +14,6 @@ jinja = SanicJinja2(app, pkg_name="main")
 app.static('/static', './static')
 
 
-# todo сделать проверку поступившего изображения по имени
 @app.route(methods=['GET', 'POST'], uri='/upload')
 def upload_img(request):
     template = open(os.getcwd() + "/templates/index.html")
@@ -20,15 +22,16 @@ def upload_img(request):
         byte_image = args.getlist('file')[0][1]
         original_image = Image.open(io.BytesIO(byte_image))
         original_image.thumbnail((256, 256), Image.ANTIALIAS)
-        # if(os.path.isfile("./static/img/" + str(uuid.uuid4()) + ".png")):
         original_image.save("./static/img/" + str(uuid.uuid4()) + ".png")
+
     return response.html(template.read())
+
 
 @app.route(methods=['GET', 'POST'], uri='/images')
 def return_img(request):
     filenames = os.listdir(os.getcwd() + "/static/img/")
-    for elem in filenames:
-        return jinja.render("return_image.html", request, img_path="/static/img/" + elem)
+    print(filenames)
+    return jinja.render("return_image.html", request, imgpath="/static/img/", filenames=filenames)
 
 
 app.run(host="0.0.0.0", port=8000, debug=True)
